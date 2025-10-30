b6cd6ae111811d5f2289da32425be8af839d50d1
Sparsha <129596934+sparshabhusal@users.noreply.github.com>
2025-10-30 21:27:31 +0545

    Delete Config directory

--- Config/hypr/scripts/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# ----- ⭐ https://github.com/sparshabhusal ⭐ ----- #
-
-swww-daemon &
-swww img ~/Pictures/Wallpapers/nord.png --transition-type none &
-wal -i ~/Pictures/Wallpapers/nord.png
-wal -R
-
