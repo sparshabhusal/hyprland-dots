27038e2a9ff48861c8927df9a00a9ee8dd86a838
Sparsha <129596934+sparshabhusal@users.noreply.github.com>
2025-10-31 20:43:31 +0545

    Create packages.sh

---
+++ installers/packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Packages
+sudo pacman -S git curl wget
+
+# Font packages
+yay -S ttf-nerd-fonts-symbols ttf-jetbrains-mono-nerd
+
+# Utilities
+yay -S kitty rofi pavucontrol wlogout waybar
+
+# Terminal utiltities
+yay -S tty-clock cmatrix cava btop htop
